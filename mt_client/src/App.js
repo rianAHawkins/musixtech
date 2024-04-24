af7641c4527e2d8450c99e60dad9c6194b9e8b62
rianAHawkins <rianandrewwhite@gmail.com>
2024-04-24 14:33:03 -0400

    webpack mt

---
+++ mt_client/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+import {
+  Route, Routes
+} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+    
+  );
+}
+
+export default App;
---
+++ mt_client/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div>
+            <p>This is a home page</p>
+            {console.log("You made it!")}
+        </div>
+    )
+};
+
+export default Home;
